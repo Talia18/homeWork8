bd54bc091a18bf93e6e9da93908a3d9d372828a5
Talia18 <taliavaknnin@gmail.com>
2022-09-07 21:37:59 +0300

    home work 8

---
+++ scirpt.js
@@ -0,0 +1,11 @@
+let sh = [
+  ["white", "black", "white", "black", "white", "black", "white", "black"],
+  ["black", "white", "black", "white", "black", "white", "black", "white"],
+  ["white", "black", "white", "black", "white", "black", "white", "black"],
+  ["black", "white", "black", "white", "black", "white", "black", "white"],
+  ["white", "black", "white", "black", "white", "black", "white", "black"],
+  ["black", "white", "black", "white", "black", "white", "black", "white"],
+  ["white", "black", "white", "black", "white", "black", "white", "black"],
+  ["black", "white", "black", "white", "black", "white", "black", "white"],
+];
+console.log(sh);
