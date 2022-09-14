52ab8f090a90c0ed5f9678f5ad7e0eb2e0b2cb58
Talia18 <taliavaknnin@gmail.com>
2022-09-14 22:19:55 +0300

    home work 8

--- scirpt.js
+++ scirpt.js
@@ -1,11 +1,20 @@
-let sh = [
-  ["white", "black", "white", "black", "white", "black", "white", "black"],
-  ["black", "white", "black", "white", "black", "white", "black", "white"],
-  ["white", "black", "white", "black", "white", "black", "white", "black"],
-  ["black", "white", "black", "white", "black", "white", "black", "white"],
-  ["white", "black", "white", "black", "white", "black", "white", "black"],
-  ["black", "white", "black", "white", "black", "white", "black", "white"],
-  ["white", "black", "white", "black", "white", "black", "white", "black"],
-  ["black", "white", "black", "white", "black", "white", "black", "white"],
-];
-console.log(sh);
+let sch = new Array([], [], [], [], [], [], [], []);
+for (let i = 0; i <= 7; i++) {
+  for (let z = 0; z <= 7; z++) {
+    if (i % 2 == 0) {
+      if (z % 2 == 0) {
+        sch[i].push("black");
+      } else {
+        sch[i].push("white");
+      }
+    } else {
+      if (z % 2 == 0) {
+        sch[i].push("white");
+      } else {
+        sch[i].push("black");
+      }
+    }
+  }
+}
+
+console.log(sch);
